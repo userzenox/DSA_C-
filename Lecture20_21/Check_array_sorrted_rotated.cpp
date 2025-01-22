52b5b535b344ee94ca80544c4d37e57a718d81b9
userzenox <piyush.132kp@gmail.com>
2025-01-22 19:29:48 +0530

    commit-day-22

---
+++ Lecture20_21/Check_array_sorrted_rotated.cpp
@@ -0,0 +1,39 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void print(int array[], int size){
+     for(int i=0; i<size; i++){
+        cout << array[i] << " ";
+     }
+     cout  << endl;
+}
+
+bool check(int arr[], int n){
+    int count=0;
+    for( int i=0;i<n; i++){
+        if(arr[i] > arr[i+1]){
+                count++;
+        }
+    }
+    if(arr[n-1] > arr[0]){
+        count++;
+    }
+    if(count>1){
+        return false;
+    }
+    return true;
+}
+
+
+int main(){
+
+    int arr[5] = {1,1,1,1,1};
+
+    print(arr,5);
+
+     cout << endl << "is is sorted array " << check(arr,5);
+     return 0;
+}
