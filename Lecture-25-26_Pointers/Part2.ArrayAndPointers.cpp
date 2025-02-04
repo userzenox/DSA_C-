6b2021fa165dd546a1365b29156b2be91a2c9d0f
userzenox <piyush.132kp@gmail.com>
2025-02-04 19:55:28 +0530

    coomit-feb-4

---
+++ Lecture-25-26_Pointers/Part2.ArrayAndPointers.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[10]={2,3,4,5};
+
+    cout <<"address of first memory block is" << arr << endl;
+
+    cout <<"address of first memory block is" << &arr[0] << endl;
+
+
+    
+    cout << "Value at zeroth location of *arr - " << *arr << endl;
+    cout << "Value of *arr + 1 - " << *arr + 15 << endl;
+    cout << "Value at first location of *(arr + 1) - " << *(arr +1) << endl;
+
+    //array can be also written as 
+     int i=2;
+      cout << "arr is the array " << i[arr] << endl;
+      cout << "arr is the array " << *(i+ arr)<< endl;
+
+      int *p = &arr[0];
+      cout << "value of *p " << *p <<endl; 
+      cout << "value of p " << p << "  it is the address of value stored"<<endl;
+      cout << "value of &p " << &p <<endl;
+      cout << "value of arr " << arr <<endl;
+      cout << "value of &arr " << &arr <<endl;
+      cout << "value of *arr " << *arr <<endl;
+
+
+      int temp[10] = {1,2};
+cout << sizeof(temp) << endl;
+cout << " 1st " << sizeof(*temp) << endl;
+cout << " 2nd " << sizeof(&temp) << endl;
+
+int *ptr = &temp[0];
+cout << sizeof(ptr) << endl;
+cout << sizeof(*ptr) << endl;
+cout << sizeof(&ptr) << endl;
+
+    return 0;
+}
