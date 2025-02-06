6e5ca7b3203f0fd31dbff041cefc0052f5cdb4d2
userzenox <piyush.132kp@gmail.com>
2025-02-06 19:04:17 +0530

    commit-6

--- Lecture-25-26_27_Pointers/Double_Pointer.cpp
+++ Lecture-25-26_27_Pointers/Double_Pointer.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+void update(int **p){
+    // p=p+1;  //no change
+
+    *p = *p +1;
+
+    // **p = **p +1;
+}
+
 int main(){
     
     int i = 5;
@@ -8,32 +16,41 @@ int main(){
     int **p2 = &p;
 
 
-    cout << "printing p - " << p << endl;
-    cout << "printing *p2 - " << *p2 << endl;
-    cout << "printing p2 - " << p2 << endl;
-    cout << "address of &p - " << &p << endl;
+    // cout << "printing p - " << p << endl;
+    // cout << "printing *p2 - " << *p2 << endl;
+    // cout << "printing p2 - " << p2 << endl;
+    // cout << "address of &p - " << &p << endl;
 
 
-    //printing value of i
-    cout << "printing value of i" << endl;
-    cout << i << endl;
-    cout << *p << endl;
-    cout << **p2 << endl;
+    // //printing value of i
+    // cout << "printing value of i" << endl;
+    // cout << i << endl;
+    // cout << *p << endl;
+    // cout << **p2 << endl;
 
 
 
-    cout << "printing address of i" << endl;
-    cout << &i << endl;
-    cout << p << endl;
-    cout << *p2 << endl;
+    // cout << "printing address of i" << endl;
+    // cout << &i << endl;
+    // cout << p << endl;
+    // cout << *p2 << endl;
 
 
     
-    cout << "printing address of p" << endl;
-    cout << &p << endl;
-    cout << p2 << endl;
+    // cout << "printing address of p" << endl;
+    // cout << &p << endl;
+    // cout << p2 << endl;
   
+      cout << endl;
+      cout << "before " << i << endl;
+      cout << "before " << p << endl;
+      cout << "before " << p2 << endl;
 
+      update(p2);
+      
+      cout << "After " << i << endl;
+      cout << "After " << p << endl;
+      cout << "After " << p2 << endl;
 
 
 
