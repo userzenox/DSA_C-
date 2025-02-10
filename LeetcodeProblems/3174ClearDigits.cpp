c2943a9d0d4a7f81e056623e90c4c612bb55a2ca
userzenox <piyush.132kp@gmail.com>
2025-02-10 21:35:26 +0530

    correctedthecode

--- LeetcodeProblems/3174ClearDigits.cpp
+++ LeetcodeProblems/3174ClearDigits.cpp
@@ -1,47 +1,33 @@
-// You are given a string s.
-
-// Your task is to remove all digits by doing this operation repeatedly:
-
-// Delete the first digit and the closest non-digit character to its left.
-// Return the resulting string after removing all digits.
-
-
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 class Solution {
     public:
         string clearDigits(string s) {
-            int count1=0,count2=0;
-            for(int i=0; i< s.size(); i++){
-                if(s[i] >= 48 && s[i] <= 57 ){
-                    int k =i+1, j=i+1;
-    
-                    
-                    while (s[j] >= 'a' && s[j] <= 'z' && k< s.size()){
-                        k++;
-                        count1++;
-                       }
-                    while( s[j] >= 'a' && s[j] <= 'z' && j>0){
-                        j--;
-                        count2++;
-                       }   
-    
-                if(count1 > count2){
-                     s.erase(s.begin()+i,s.begin()+j);
-                }else{
-                     s.erase(s.begin()+i,s.begin()+k);
-                }   
+            while (true) {
+                bool found = false;
+                for (int i = 0; i < s.size(); i++) {
+                    if (s[i] >= '0' && s[i] <= '9') {
+                        // Find the closest non-digit character to the left
+                        for (int j = i - 1; j >= 0; j--) {
+                            if (!(s[j] >= '0' && s[j] <= '9')) {
+                                s.erase(s.begin() + i); // Remove the digit
+                                s.erase(s.begin() + j); // Remove the closest non-digit character to the left
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found) break;
+                    }
                 }
+                if (!found) break; // No more digits found, exit the loop
             }
             return s;
         }
     };
 
-
+    
 int main() {
     Solution solution;
     string input = "abc1def2gh3";
