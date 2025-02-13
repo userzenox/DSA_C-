9de294e93c2064fe72fd4bfc98c1b50473ea01ca
userzenox <piyush.132kp@gmail.com>
2025-02-13 22:48:13 +0530

    feb-13

---
+++ Lecture-31_Recursion/Climb_the_stair_case.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int climb(int n){
+
+    
+}
+
+
+
+int main(){
+
+    int n;
+    cout<< "Enter the number" << endl;
+    cin >> n;
+
+     cout << climb(n);
+
+    return 0;
+}
