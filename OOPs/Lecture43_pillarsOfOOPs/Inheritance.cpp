c50b74e5653a66759df94b3ec05d905d2ef90ced
userzenox <piyush.132kp@gmail.com>
2025-04-07 21:48:12 +0530

    commit-7

---
+++ OOPs/Lecture43_pillarsOfOOPs/Inheritance.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Human {
+public:
+   
+    int age;
+    int weight;
+protected:    
+    int height=5.8;
+   
+
+public:
+    int getAge() {
+        return this->age;
+    }
+    void setWeight(int w) {
+        this->weight = w;
+    }
+};
+
+class Male: public Human {
+
+    public: 
+    string colour;
+
+    void sleep(){
+        cout << "Male sleeping" << endl;
+    }
+    int getheight(){
+        return this-> height;
+    }
+};
+
+int main() {
+
+    Male object1;
+    cout << object1.age << endl;
+    cout << object1.colour << endl;
+    cout << object1.weight << endl;
+
+    object1.setWeight(60);
+    cout << object1.weight << endl;
+    cout << object1.getheight() << endl;
+
+    
+    return 0;
+}
