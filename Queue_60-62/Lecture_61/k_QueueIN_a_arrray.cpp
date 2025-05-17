45723274b009a4450f4df4152104b4c7957e80d6
userzenox <piyush.132kp@gmail.com>
2025-05-17 12:37:38 +0530

    commit_codecorrect

--- Queue_60-62/Lecture_61/k_QueueIN_a_arrray.cpp
+++ Queue_60-62/Lecture_61/k_QueueIN_a_arrray.cpp
@@ -1,14 +1,16 @@
 // C++ program to implement k queues 
-// in a single array (Naive Approach)
+// in a single array (Efficient Approach)
 #include <bits/stdc++.h>
 using namespace std;
 
 class kQueues {
 private:
-    vector<int> arr;       
-    int n, k;             
-    vector<int> front;    
-    vector<int> rear;      
+    vector<int> arr;   
+    int n, k;          
+    vector<int> front; 
+    vector<int> rear;  
+    vector<int> next;  
+    int freeIndex;          
 
 public:
     kQueues(int n, int k) {
@@ -17,6 +19,15 @@ public:
         arr.resize(n);
         front.resize(k, -1);
         rear.resize(k, -1);
+        next.resize(n);
+        
+        // Initialize all spaces as free
+        freeIndex = 0;
+        for (int i = 0; i < n-1; i++)
+            next[i] = i + 1;
+            
+        // -1 is used to indicate end of free list
+        next[n-1] = -1; 
     }
     
     // Function to check if queue 'qn' is empty
@@ -24,33 +35,40 @@ public:
         return (front[qn] == -1);
     }
     
-    // Function to check if queue 'qn' is full
-    bool isFull(int qn) {
-        // Calculate the next position after rear
-        int nextPos = (rear[qn] + 1) % (n/k);
-        // Queue is full if next position is front
-        return (nextPos == front[qn] % (n/k) && front[qn] != -1);
+    // Function to check if array is full
+    bool isFull() {
+        return (freeIndex == -1);
     }
     
     // Function to enqueue 'x' into queue 'qn'
     bool enqueue(int x, int qn) {
         
-        // Check if queue is full
-        if (isFull(qn))
+        // Check if array is full
+        if (isFull())
             return false;
             
-        // Calculate the base index for this queue
-        int base = qn * (n/k);
+        // Get next free index
+        int i = freeIndex;
+        freeIndex = next[i];
         
-        // For empty queue, set front index
+        // If queue is empty, update 
+        // both front and rear
         if (isEmpty(qn))
-            front[qn] = base;
+            front[qn] = i;
+        else {
+            
+            // Link new element to the previous rear
+            next[rear[qn]] = i; 
+        }
         
-        // Update rear and add element
-        rear[qn] = (rear[qn] == -1) ? 
-            base : 
-            base + (rear[qn] + 1 - base) % (n/k);
-        arr[rear[qn]] = x;
+        // Update rear
+        rear[qn] = i;
+        
+        // Store the element
+        arr[i] = x;
+        
+        // Mark end of queue
+        next[i] = -1;
         
         return true;
     }
@@ -62,24 +80,22 @@ public:
         if (isEmpty(qn))
             return -1;
             
-        // Get the base index for this queue
-        int base = qn * (n/k);
+        // Get the front index of queue
+        int i = front[qn];
         
-        // Get the element to be dequeued
-        int x = arr[front[qn]];
+        // Update front
+        front[qn] = next[i];
         
-        // If this is the last element, reset front and rear
-        if (front[qn] == rear[qn]) {
-            front[qn] = -1;
+        // If queue becomes empty
+        if (front[qn] == -1)
             rear[qn] = -1;
-        } else {
-            
-            // Move front to next position, 
-            // considering wrap-around within segment
-            front[qn] = base + (front[qn] + 1 - base) % (n/k);
-        }
         
-        return x;
+        // Add the dequeued position to free list
+        next[i] = freeIndex;
+        freeIndex = i;
+        
+        // Return the dequeued element
+        return arr[i];
     }
 };
 
@@ -91,11 +107,15 @@ int main() {
     cout << queues.enqueue(20, 1) << " "; 
     cout << queues.enqueue(30, 0) << " "; 
     cout << queues.enqueue(40, 2) << " "; 
+    cout << queues.enqueue(50, 1) << " "; 
+    cout << queues.enqueue(60, 0) << " "; 
+    cout << queues.enqueue(70, 2) << " "; 
     
     cout << queues.dequeue(0) << " ";     
     cout << queues.dequeue(1) << " ";     
     cout << queues.dequeue(2) << " ";     
     cout << queues.dequeue(0) << " ";     
+    cout << queues.enqueue(80, 0) << " "; 
     cout << queues.dequeue(0) << " ";     
     
     return 0;
