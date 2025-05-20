e516aa7b97f804c70c31a22b0920b6b2bd701b5c
userzenox <piyush.132kp@gmail.com>
2025-05-20 21:37:00 +0530

    commit-20

---
+++ Binary_Tree/Lecture_62/creation_BT.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class node{
+   public: 
+         int data;
+         node* left;
+         node* right;
+
+         node(int d){
+            this -> data = d;
+            this -> left = NULL;
+            this -> right = NULL;
+         }
+};
+
+node* buildTree( node* root){
+  cout << "enter the data " << endl;
+  int data ;
+  cin>> data;
+  root =new node(data);
+
+  if(data == -1){
+     return NULL;
+  }
+  
+  cout<< " enter data for inserting in left" << data <<endl;
+  root->left = buildTree(root->left);
+  cout << " enter data for inserting in right"  << data<< endl;
+root -> right = buildTree(root->right);
+return root;
+}
+
+int main() {
+    
+    node* root = NULL;
+
+    root = buildTree(root);
+
+}
