7b063dd28dbba97869bdcec72249be44baf0fd85
userzenox <piyush.132kp@gmail.com>
2025-05-23 22:05:07 +0530

    commit-23

---
+++ Binary_Tree/Lecture_62/InOrder_NonRecursive.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+
+        while (curr != nullptr || !st.empty()) {
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            curr = curr->right;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    // Manually creating this binary tree:
+    //       1
+    //        \
+    //         2
+    //        /
+    //       3
+
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution sol;
+    vector<int> result = sol.inorderTraversal(root);
+
+    cout << "Inorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Free allocated memory
+    delete root->right->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
